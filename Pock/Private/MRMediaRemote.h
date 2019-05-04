0cd98e36897f88d9e6b8c5953f4b678cec673363
Pierluigi Galdi <pigi.galdi@gmail.com>
2019-05-04 15:44:42 +0200

    Remove unneeded stuff

--- Pock/Private/MRMediaRemote.h
+++ Pock/Private/MRMediaRemote.h
@@ -10,10 +10,10 @@ typedef void (^MRMediaRemoteGetNowPlayingInfoBlock)(NSDictionary *info);
 typedef void (^MRMediaRemoteGetNowPlayingClientBlock)(id clientObj);
 typedef void (^MRMediaRemoteGetNowPlayingApplicationIsPlayingBlock)(BOOL playing);
 
-void MRMediaRemoteRegisterForNowPlayingNotifications(dispatch_queue_t queue);
-void MRMediaRemoteGetNowPlayingClient(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingClientBlock block);
-void MRMediaRemoteGetNowPlayingInfo(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingInfoBlock block);
-void MRMediaRemoteGetNowPlayingApplicationIsPlaying(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingApplicationIsPlayingBlock block);
+extern void MRMediaRemoteRegisterForNowPlayingNotifications(dispatch_queue_t queue);
+extern void MRMediaRemoteGetNowPlayingClient(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingClientBlock block);
+extern void MRMediaRemoteGetNowPlayingInfo(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingInfoBlock block);
+extern void MRMediaRemoteGetNowPlayingApplicationIsPlaying(dispatch_queue_t queue, MRMediaRemoteGetNowPlayingApplicationIsPlayingBlock block);
 
 extern NSString *MRNowPlayingClientGetBundleIdentifier(id clientObj);
 extern NSString *MRNowPlayingClientGetParentAppBundleIdentifier(id clientObj);
@@ -29,34 +29,11 @@ extern NSString *kMRMediaRemoteNowPlayingInfoArtist;
 extern NSString *kMRMediaRemoteNowPlayingInfoTitle;
 
 typedef enum {
-    /*
-     * Use nil for userInfo.
-     */
     kMRPlay = 0,
     kMRPause = 1,
     kMRTogglePlayPause = 2,
-    kMRStop = 3,
     kMRNextTrack = 4,
     kMRPreviousTrack = 5,
-    kMRToggleShuffle = 6,
-    kMRToggleRepeat = 7,
-    kMRStartForwardSeek = 8,
-    kMREndForwardSeek = 9,
-    kMRStartBackwardSeek = 10,
-    kMREndBackwardSeek = 11,
-    kMRGoBackFifteenSeconds = 12,
-    kMRSkipFifteenSeconds = 13,
-    
-    /*
-     * Use a NSDictionary for userInfo, which contains three keys:
-     * kMRMediaRemoteOptionTrackID
-     * kMRMediaRemoteOptionStationID
-     * kMRMediaRemoteOptionStationHash
-     */
-    kMRLikeTrack = 0x6A,
-    kMRBanTrack = 0x6B,
-    kMRAddTrackToWishList = 0x6C,
-    kMRRemoveTrackFromWishList = 0x6D
 } MRCommand;
 
 extern Boolean MRMediaRemoteSendCommand(MRCommand command, id userInfo);
