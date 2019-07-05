e4904403cf495a1264a5a4c57071840c4420aadd
L1cardo <1014660822@qq.com>
2019-07-05 16:05:35 +0800

    added QQ music support, which is also commonly used in China

--- Pock/Widgets/NowPlaying/NowPlayingItemView.swift
+++ Pock/Widgets/NowPlaying/NowPlayingItemView.swift
@@ -36,7 +36,7 @@ class NowPlayingItemView: PKDetailView {
         switch (appBundleIdentifier) {
         case "com.apple.WebKit.WebContent":
             appBundleIdentifier = "com.apple.Safari"
-        case "com.spotify.client", "com.apple.iTunes", "com.apple.Safari","com.netease.163music":
+        case "com.spotify.client", "com.apple.iTunes", "com.apple.Safari","com.netease.163music","com.tencent.QQMusicMac":
             break
         default:
             appBundleIdentifier = "com.apple.iTunes"
