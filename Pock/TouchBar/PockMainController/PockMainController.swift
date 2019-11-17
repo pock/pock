9bcfb1686629a2253ba59df9837d15821c346271
Pierluigi Galdi <pigi.galdi@gmail.com>
2019-11-17 12:27:16 +0100

    Refactor for PockKit 3rd-party widget integration

--- Pock/TouchBar/PockMainController/PockMainController.swift
+++ Pock/TouchBar/PockMainController/PockMainController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import PockKit
 
 /// Custom identifiers
 extension NSTouchBar.CustomizationIdentifier {
@@ -23,9 +24,6 @@ extension NSTouchBarItem.Identifier {
 
 class PockMainController: PKTouchBarController {
     
-    /// Core
-    private var loadedWidgets: [NSTouchBarItem.Identifier: PKWidget] = [:]
-    
     override var systemTrayItem: NSCustomTouchBarItem? {
         let item = NSCustomTouchBarItem(identifier: .pockSystemIcon)
         item.view = NSButton(image: #imageLiteral(resourceName: "pock-inner-icon"), target: self, action: #selector(presentFromSystemTrayItem))
@@ -34,52 +32,21 @@ class PockMainController: PKTouchBarController {
     override var systemTrayItemIdentifier: NSTouchBarItem.Identifier? { return .pockSystemIcon }
     
     deinit {
-        self.loadedWidgets.removeAll()
+        WidgetsDispatcher.default.clearLoadedWidgets()
         if !isProd { print("[PockMainController]: Deinit Pock main controller") }
     }
     
-    private func loadPluginsFromFilesystem(completion: ([PKWidget]) -> Void) {
-        self.loadedWidgets.removeAll()
-        
-        let path = FileManager.default.homeDirectoryForCurrentUser.path + "/Library/Application Support/Pock"
-        let widgetsPath = path + "/Widgets"
-        var directoryExists: ObjCBool = false
-        if !FileManager.default.fileExists(atPath: path) {
-            try? FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: false, attributes: nil)
-            try? FileManager.default.createDirectory(atPath: widgetsPath, withIntermediateDirectories: false, attributes: nil)
-        }
-        FileManager.default.fileExists(atPath: path, isDirectory: &directoryExists)
-        FileManager.default.fileExists(atPath: widgetsPath, isDirectory: &directoryExists)
-        guard directoryExists.boolValue else {
-            completion([])
-            return
-        }
-        let enumerator = FileManager.default.enumerator(atPath: widgetsPath)
-        let widgetBundles = (enumerator?.allObjects as? [String] ?? []).filter{ $0.contains(".pock") && !$0.contains("/") }
-        for widgetBundle in widgetBundles {
-            /// load bundle
-            let bundlePath = "\(widgetsPath)/\(widgetBundle)"
-            if let bundle = Bundle(path: bundlePath), bundle.load() {
-                if let clss = bundle.principalClass as? PKWidget.Type {
-                    let plugin = clss.init()
-                    self.loadedWidgets[plugin.identifier] = plugin
-                }
-            }
-        }
-        completion(Array(loadedWidgets.values))
-    }
-    
-    override func awakeFromNib() {
-        self.loadPluginsFromFilesystem(completion: { widgets in
+    override func didLoad() {
+        WidgetsDispatcher.default.loadInstalledWidget() { widgets in
             self.touchBar?.customizationIdentifier              = .pockTouchBar
             self.touchBar?.defaultItemIdentifiers               = [.escButton, .dockView]
             self.touchBar?.customizationAllowedItemIdentifiers  = [.escButton, .dockView, .controlCenter, .nowPlaying, .status]
-            
+
             let customizableIds: [NSTouchBarItem.Identifier] = widgets.map({ $0.identifier })
             self.touchBar?.customizationAllowedItemIdentifiers.append(contentsOf: customizableIds)
-            
+
             super.awakeFromNib()
-        })
+        }
     }
     
     func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
@@ -101,7 +68,7 @@ class PockMainController: PKTouchBarController {
         case .status:
             widget = StatusWidget()
         default:
-            widget = loadedWidgets[identifier]
+            widget = WidgetsDispatcher.default.loadedWidgets[identifier]
         }
         guard widget != nil else { return nil }
         return PKWidgetTouchBarItem(widget: widget!)
