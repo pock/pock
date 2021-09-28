9339391f01be7812789490cbe702ddb8cbfdf42b
Pierluigi Galdi <pigi.galdi@gmail.com>
2021-09-28 21:52:54 +0200

    prog: remove legacy sources

--- appcenter-pre-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Create Secrets.plist file
-if [ "$APPCENTER_BRANCH" == "master" ];
-then
-    defaults write $APPCENTER_SOURCE_DIRECTORY/Pock/Various/Secrets.plist AppCenter $APPCENTER_SECRET
-fi
